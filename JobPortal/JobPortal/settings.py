3ffb157d7cec42f1f86d6f6db021bde836de41c5
SumeetPatil7 <sumitpatil1299@gmail.com>
2021-01-15 17:26:39 +0530

    Minor Modification

--- JobPortal/JobPortal/settings.py
+++ JobPortal/JobPortal/settings.py
@@ -126,6 +126,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/templates/'
+STATIC_URL = '/static/'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
